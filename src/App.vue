e8637b3a6811a81aba8efe16d36afeb702b1fd02
melpst <jitlada.su@ku.th>
2019-10-16 11:24:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <EmployeeForm @add-employee="addEmployee"/>
+    <EmployeeTable v-bind:employees="employees" />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
+export default {
+  name: 'app',
+  components: {
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+      this.employees = [...this.employees, {...employee, id: this.employees.length+1}]
+      alert(this.employees.length)
+    }
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input v-model="employee.name" type="text" />
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+    methods:{
+        handleSubmit(){
+            if(this.isInvalidName|| this.isInvalidEmail){
+                alert('please fill the form')
+            }
+            else{
+                this.$emit('add-employee', this.employee)
+            }
+        }
+    },
+    computed:{
+        isInvalidName(){
+            return this.employee.name.replace(/ /g, '')===''
+        },
+        isInvalidEmail(){
+            return this.employee.email.replace(/ /g, '')===''
+        }
+    }
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
